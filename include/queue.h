5d61f6f0a6012fca5bf16118a66128a6d7199f98
Xuann <yinxuan99@outlook.com>
2021-04-11 18:28:32 +0800

    complete queue.h

--- include/queue.h
+++ include/queue.h
@@ -108,7 +108,14 @@
  * already in the list.  The "field" name is the link element
  * as above.
  */
-#define LIST_INSERT_AFTER(listelm, elm, field)
+#define LIST_INSERT_AFTER(listelm, elm, field) do {
+		LIST_NEXT((elm),field) = LIST_NEXT((listelm),field);
+		if (LIST_NEXT((listelm),field) !=  null) {
+			LIST_NEXT((listelm),field)->field.le_prev = &LIST_NEXT((elm),field);
+		}
+		LIST_NEXT((listelm),field) = (elm);
+		elm->field.le_prev = &LIST_NEXT((listelm),field);
+	} while(0)
         // Note: assign a to b <==> a = b
         //Step 1, assign elm.next to listelem.next.
         //Step 2: Judge whether listelm.next is NULL, if not, then assign listelm.pre to a proper value.
@@ -144,7 +151,11 @@
  * The "field" name is the link element as above. You can refer to LIST_INSERT_HEAD.
  * Note: this function has big differences with LIST_INSERT_HEAD !
  */
-#define LIST_INSERT_TAIL(head, elm, field)
+#define LIST_INSERT_TAIL(head, elm, field) do {
+		LIST_NEXT((elm),field) = LIST_NEXT((listelm),field);
+		LIST_NEXT((listelm),field) = (elm);
+		(elm)->field.le_prev = &LIST_NEXT((listelm),field);
+	} while(0)	
 /* finish your code here. */
 
 
